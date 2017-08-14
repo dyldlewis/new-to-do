033d76d84f54bea1d7abb3aee2d9bc26c864012e
Calla Rudolph and Nathan Stewart <nathan@>
2017-08-14 14:45:29 -0700

    hide edit form, bind to click event

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
       <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit$</button></li>
     </ul>
     <hr>
-    <div>
+    <div *ngIf="selectedTask">
       <h3>{{selectedTask.description}}</h3>
       <p>Task Complete? {{selectedTask.done}}</p>
       <h3>Edit Task</h3>
@@ -21,6 +21,7 @@ import { Component } from '@angular/core';
       <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
       <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
       <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+      <button (click)="finishedEditing()">Done</button>
     </div>
   </div>
   `
@@ -36,7 +37,7 @@ export class AppComponent {
     new Task('Finish weekend Angular homework for Epicodus course.', 3),
     new Task('Begin brainstorming possible JavaScript group projects.', 2),
     new Task('Add README file to last few Angular repos on GitHub.', 2) ];
-  selectedTask: Task = this.tasks[0];
+  selectedTask = null;
 
   editTask(clickedTask){
     this.selectedTask = clickedTask;
@@ -58,6 +59,10 @@ export class AppComponent {
     } else {
       return "bg-info";
     }
+  }
+
+  finishedEditing(){
+    this.selectedTask = null;
   }
 }
 
