ba577000e07ae477794674de555e549a856d9323
Dylan Lewis and Nathan Stewart <dylan@>
2017-08-15 10:25:54 -0700

    set child/parent relationship between task-list and app components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Task } from './task.model';
 
 @Component({
@@ -8,7 +8,7 @@ import { Task } from './task.model';
     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
     <h3>{{currentFocus}}</h3>
     <hr>
-    <task-list></task-list>
+    <task-list [childTaskList]="masterTaskList"> </task-list>
     <div *ngIf="selectedTask">
       <h3>{{selectedTask.description}}</h3>
       <p>Task Complete? {{selectedTask.done}}</p>
@@ -34,11 +34,18 @@ export class AppComponent {
   year: number = this.currentTime.getFullYear();
   selectedTask = null;
 
-  editTask(clickedTask){
+  masterTaskList: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
+  ];
+
+  editTask(clickedTask) {
     this.selectedTask = clickedTask;
   }
 
-  finishedEditing(){
+  finishedEditing() {
     this.selectedTask = null;
   }
+
 }
